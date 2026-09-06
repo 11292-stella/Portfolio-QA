d7de09655b690caddb70399451143bec962b608c
11292-stella <stella.marucelli@gmail.com>
2026-09-06 17:09:10 +0200

    fix

--- core/models.py
+++ core/models.py
@@ -161,3 +161,4 @@ class ContactMessage(models.Model):
 
     # per rimigrare dopo cambiamento: python manage.py makemigrations  python manage.py migrate  python manage.py runserver
     # per ativare modifiche: python manage.py collectstatic --noinput
+    # per accedere al db deploiato: 127.0.0.1:8000/admin
