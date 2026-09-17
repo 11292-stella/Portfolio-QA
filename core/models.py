9bd98bbcc7bb6c9ea007b56f8ec7bd301771a9e8
11292-stella <stella.marucelli@gmail.com>
2026-09-17 16:44:52 +0200

    fix cv

--- core/models.py
+++ core/models.py
@@ -162,3 +162,4 @@ class ContactMessage(models.Model):
     # per rimigrare dopo cambiamento: python manage.py makemigrations  python manage.py migrate  python manage.py runserver
     # per ativare modifiche: python manage.py collectstatic --noinput
     # per accedere al db deploiato: https://portfolio-qa-8f6q.onrender.com/admin/
+    # per modifiche e rilancio: python manage.py collectstatic --noinput
